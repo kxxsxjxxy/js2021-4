8e70715d12a37bd699a464f69c52121cdd2d15c4
kxxsxjxxy <kxxsxjxxy@naver.com>
2021-03-23 12:47:42 +0900

    Hello.js

--- hello.js
+++ hello.js
@@ -1,5 +1,13 @@
-/*
-console.log('Hello World...!');
-console.log('Hello World...!');
-console.log('Hello World...!');
-*/
+let date = new Date();
+
+console.log(date.getFullYear());
+console.log(date.getMonth());
+console.log(date.getDay());
+console.log(date.getTime());
+console.log(date.getMinutes());
+console.log(date.getSeconds());
+
+if(date.getHours() < 12) {
+    console.log("오전!");
+}
+console.log("오후!");
